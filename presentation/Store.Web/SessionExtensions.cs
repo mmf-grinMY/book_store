499ae09272958e38e4b85533f94e9083b1309f39
MMF-grinMY-bsu <mmf.grinmy@mail.ru>
2022-11-17 05:01:30 +0300

    Добавить заказы и храналище заказов

--- presentation/Store.Web/SessionExtensions.cs
+++ presentation/Store.Web/SessionExtensions.cs
@@ -15,15 +15,19 @@ namespace Store.Web
             using (var stream = new MemoryStream())
             using (var writer = new BinaryWriter(stream, Encoding.UTF8, true))
             {
-                writer.Write(value.Items.Count);
+                writer.Write(value.OrderId);
+                writer.Write(value.TotalCount);
+                writer.Write(value.TotalPrice);
 
-                foreach (var item in value.Items)
-                {
-                    writer.Write(item.Key);
-                    writer.Write(item.Value);
-                }
+                //writer.Write(value.Items.Count);
 
-                writer.Write(value.Amount);
+                //foreach (var item in value.Items)
+                //{
+                //    writer.Write(item.Key);
+                //    writer.Write(item.Value);
+                //}
+
+                //writer.Write(value.Amount);
 
                 session.Set(key, stream.ToArray());
             }
@@ -36,18 +40,28 @@ namespace Store.Web
                 using (var stream = new MemoryStream(buffer))
                 using (var reader = new BinaryReader(stream, Encoding.UTF8, true))
                 {
-                    value = new Cart();
+                    //value = new Cart();
 
-                    var length = reader.ReadInt32();
-                    for (int i = 0; i < length; i++)
+                    //var length = reader.ReadInt32();
+                    //for (int i = 0; i < length; i++)
+                    //{
+                    //    var bookId = reader.ReadInt32();
+                    //    var count = reader.ReadInt32();
+
+                    //    value.Items.Add(bookId, count);
+                    //}
+
+                    //value.Amount = reader.ReadInt32();
+
+                    var orderId = reader.ReadInt32();
+                    var totalCount = reader.ReadInt32();
+                    var totalPrice = reader.ReadDecimal();
+
+                    value = new Cart(orderId)
                     {
-                        var bookId = reader.ReadInt32();
-                        var count = reader.ReadInt32();
-
-                        value.Items.Add(bookId, count);
-                    }
-
-                    value.Amount = reader.ReadInt32();
+                        TotalCount = totalCount,
+                        TotalPrice = totalPrice,
+                    };
 
                     return true;
                 }
